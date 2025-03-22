c32c9c60924ecd5590bd523c942402095d5ee67e
priiii08918 <priyanshusinghal0818@proton.me>
2025-03-22 14:46:31 +0000

    Revert "cpufreq: schedutil: Simplify sugov_update_next_freq()"
    This reverts commit 9d091e874b660fb70feb5e69ac34c66fcda4eea5.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -124,10 +124,12 @@ static bool sugov_up_down_rate_limit(struct sugov_policy *sg_policy, u64 time,
 static bool sugov_update_next_freq(struct sugov_policy *sg_policy, u64 time,
 				   unsigned int next_freq)
 {
-	if (sg_policy->need_freq_update)
+	if (!sg_policy->need_freq_update) {
+		if (sg_policy->next_freq == next_freq)
+			return false;
+	} else {
 		sg_policy->need_freq_update = false;
-	else if (sg_policy->next_freq == next_freq)
-		return false;
+	}
 
 	if (sugov_up_down_rate_limit(sg_policy, time, next_freq))
 		return false;
